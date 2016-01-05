0ce46950d65fc953358e4e8e964cc52b2cf3b808
Paul McCrodden <info@paulmccrodden.com>
2016-01-05 14:49:40 +0900

    Added uuids for yml install config and new user notification field yml.

--- src/Controller/MessagePrivateController.php
+++ src/Controller/MessagePrivateController.php
@@ -6,6 +6,7 @@
 
 namespace Drupal\message_private\Controller;
 
+use Drupal\Core\Link;
 use Drupal\Core\Url;
 use Drupal\Core\Controller\ControllerBase;
 use Drupal\message\MessageInterface;
@@ -13,6 +14,7 @@ use Drupal\message\Entity\MessageType;
 use Drupal\message\Entity\Message;
 use Drupal\Core\DependencyInjection\ContainerInjectionInterface;
 use Drupal\message_ui\Controller\MessageViewController;
+use Drupal\message_ui\MessageAccessControlHandler;
 
 
 class MessagePrivateController extends ControllerBase implements ContainerInjectionInterface {
@@ -29,53 +31,10 @@ class MessagePrivateController extends ControllerBase implements ContainerInject
    * Display list of message types to create an instance for them.
    *
    */
-  // @todo - remove note: message_ui_create_new_message_instance_list in D7.
+  // @todo - remove note: message_private_create_new_message_instance_list in D7.
   public function getAllowedInstanceList() {
     // $access_controller = new MessageAccessControlHandler('message');
     // $allowed_types = $access_controller->userCreateMessageAccess();
-
-    /*
-    // From D7 message_private instance list override fn:
-    $items = array();
-    $allowed_types = message_ui_user_can_create_message();
-
-    if ($types = message_ui_get_types()) {
-      foreach ($types as $type => $title) {
-        if ($allowed_types || (is_array($allowed_types) && $allowed_types[$type])) {
-          // Create links to message create forms.
-          if ($type != 'private_message') {
-            // @FIXME
-// l() expects a Url object, created from a route name or external URI.
-// $items[] = l($title, 'admin/content/message/create/' . str_replace('_', '-', $type));
-
-          }
-          else {
-            // Create link to customised menu item for private_message create.
-            // @FIXME
-// l() expects a Url object, created from a route name or external URI.
-// $items[] = l($title, 'message/create/' . str_replace('_', '-', $type));
-
-          }
-        }
-      }
-    }
-    else {
-      // @FIXME
-// url() expects a route name or an external URI.
-// return t("There are no messages types. You can create a new message type <a href='@url'>here</a>.", array('@url' => url('admin/structure/messages/add')));
-
-    }
-
-    // @FIXME
-// theme() has been renamed to _theme() and should NEVER be called directly.
-// Calling _theme() directly can alter the expected output and potentially
-// introduce security issues (see https://www.drupal.org/node/2195739). You
-// should use renderable arrays instead.
-//
-//
-// @see https://www.drupal.org/node/2195739
-// return theme('item_list', array('items' => $items));
-    */
 
     // @todo - replace this line with access controlled type list:
     $allowed_types = MessageType::loadMultiple();
@@ -106,18 +65,30 @@ class MessagePrivateController extends ControllerBase implements ContainerInject
     // Only use node types the user has access to.
     // @todo - override the path for private messages, or is there a better way?
     foreach ($this->getAllowedInstanceList() as $type => $entity) {
-      // @todo - get access control working below.
-      // \Doctrine\Common\Util\Debug::dump($this->entityManager()->getAccessControlHandler('message')->createAccess($type->id()));
-      // if ($this->entityManager()->getAccessControlHandler('message')->createAccess($type->id())) {
-      /* @var $entity MessageType */
-      $url = Url::fromUri('internal:/admin/content/messages/create/' . str_replace('_', '-', $type));
-      $items[] = array(
-        'type' => $type,
-        'name' => $entity->label(),
-        'internal_link' => \Drupal::l(ucfirst(str_replace('_', ' ', $type)), $url),
-      );
-      //\Doctrine\Common\Util\Debug::dump($content);
-      // }
+      if ($type != 'private_message') {
+        // @todo - get access control working below.
+        // \Doctrine\Common\Util\Debug::dump($this->entityManager()->getAccessControlHandler('message')->createAccess($type->id()));
+        // if ($this->entityManager()->getAccessControlHandler('message')->createAccess($type->id())) {
+        /* @var $entity MessageType */
+        $url = Url::fromUri('internal:/admin/content/messages/create/' . str_replace('_', '-', $type));
+        $items[] = array(
+          'type' => $type,
+          'name' => $entity->label(),
+          'internal_link' => Link::fromTextAndUrl(ucfirst(str_replace('_', ' ', $type)), $url),
+        );
+        //\Doctrine\Common\Util\Debug::dump($content);
+        // }
+      }
+      else {
+        // Create link to customised menu item for private_message create.
+        /* @var $entity MessageType */
+        $url = Url::fromUri('internal:/message/create/' . str_replace('_', '-', $type));
+        $items[] = array(
+          'type' => $type,
+          'name' => $entity->label(),
+          'internal_link' => Link::fromTextAndUrl(ucfirst(str_replace('_', ' ', $type)), $url),
+        );
+      }
     }
 
     // Bypass the admin/content/messages/create listing if only one content type is available.
