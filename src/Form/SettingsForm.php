7341aeced8b5dd8ded2d4378d864ead51b6738a7
mccrodp <info@opensourceopenmind.ninja>
2016-07-29 13:52:11 +0100

    Fixed Message Settings form and added composer.json.

--- src/Form/SettingsForm.php
+++ src/Form/SettingsForm.php
@@ -47,6 +47,7 @@ class SettingsForm extends ConfigFormBase {
     return [
       'message_limit',
       'email_notify',
+      'message_add_local_action',
       'default_limit',
       'default_interval',
     ];
@@ -71,7 +72,7 @@ class SettingsForm extends ConfigFormBase {
    *   The entity manager object.
    */
   public function __construct(ConfigFactoryInterface $config_factory, EntityManagerInterface $entity_manager) {
-    $this->setConfigFactory($config_factory);
+    parent::__construct($config_factory);
     $this->entityManager = $entity_manager;
   }
 
@@ -82,77 +83,76 @@ class SettingsForm extends ConfigFormBase {
     $config = $this->config('message_private.settings');
 
     // Global email notifications on/off checkbox.
-    $form[MESSAGE_PRIVATE_EMAIL_NOTIFICATIONS] = array(
-     '#type' => 'checkbox',
-     '#title' => t('Message Private Email Notifications'),
-     '#default_value' => variable_get(MESSAGE_PRIVATE_EMAIL_NOTIFICATIONS, TRUE),
-     '#description' => t('Global On / Off checkbox for emails notifying users of a new private message'),
-    );
+    $form['email_notify'] = [
+      '#type' => 'checkbox',
+      '#title' => t('Message Private Email Notifications'),
+      '#default_value' => $config->get('email_notify'),
+      '#description' => t('Global On / Off checkbox for emails notifying users of a new private message'),
+    ];
 
     // Local action links on/off checkbox.
-    $form[MESSAGE_PRIVATE_LOCAL_ACTION] = array(
+    $form['message_add_local_action'] = [
       '#type' => 'checkbox',
       '#title' => t('Disable Create a New Message local action links'),
-      '#default_value' => variable_get(MESSAGE_PRIVATE_LOCAL_ACTION, FALSE),
+      '#default_value' => $config->get('message_add_local_action'),
       '#description' => t('Disable local action links to create new message on user pages.'),
-    );
+    ];
 
-     // Role based message create limit on/off checkbox.
-     $form[MESSAGE_PRIVATE_MESSAGE_LIMIT] = array(
-         '#type' => 'checkbox',
-         '#title' => t('Limit Message Create By Role'),
-         '#default_value' => variable_get(MESSAGE_PRIVATE_MESSAGE_LIMIT, FALSE),
-         '#description' => t('Impose a message creation limit per interval. Users with multiple roles, get the highest limit from these roles'),
-     );
-
+    // Role based message create limit on/off checkbox.
+    $form['message_limit'] = [
+      '#type' => 'checkbox',
+      '#title' => t('Limit Message Create By Role'),
+      '#default_value' => $config->get('message_limit'),
+      '#description' => t('Impose a message creation limit per interval. Users with multiple roles, get the highest limit from these roles'),
+    ];
 
     // Conditional fieldset for all message limitation settings.
-    $form['interval_limit'] = array(
+    $form['interval_limit'] = [
       '#title' => t('Message interval limits'),
       '#type' => 'fieldset',
       '#collapsible' => TRUE,
       '#collapsed' => TRUE,
-      '#states' => array(
-        'invisible' => array(
-          ':input[name="' . MESSAGE_PRIVATE_MESSAGE_LIMIT . '"]' => array('checked' => FALSE),
-        ),
-      ),
-    );
+      '#states' => [
+        'invisible' => [
+          ':input[name="message_limit"]' => ['checked' => FALSE],
+        ],
+      ],
+    ];
 
     // Add a default fieldset.
-    $form['interval_limit'][MESSAGE_PRIVATE_DEFAULT_INDEX] = array(
+    $form['interval_limit'][MESSAGE_PRIVATE_DEFAULT_INDEX] = [
       '#type' => 'fieldset',
       '#title' => 'Default limit',
       '#description' => t('Applies to all roles with blank entries below'),
       '#collapsible' => TRUE,
       '#collapsed' => FALSE,
-    );
+    ];
 
-    $form['interval_limit'][MESSAGE_PRIVATE_DEFAULT_INDEX][MESSAGE_PRIVATE_DEFAULT_LIMIT] = array(
+    $form['interval_limit'][MESSAGE_PRIVATE_DEFAULT_INDEX]['default_limit'] = [
       '#type' => 'textfield',
       '#title' => t('Limit'),
-      '#default_value' => variable_get(MESSAGE_PRIVATE_DEFAULT_LIMIT),
+      '#default_value' => $config->get('default_limit'),
       '#description' => t('Enter a message limit') . ' ' . MESSAGE_PRIVATE_MESSAGE_LIMIT_MIN . ' - ' . MESSAGE_PRIVATE_MESSAGE_LIMIT_MAX,
-    );
+    ];
 
 
-     $form['interval_limit'][MESSAGE_PRIVATE_DEFAULT_INDEX][MESSAGE_PRIVATE_DEFAULT_INTERVAL] = array(
+     $form['interval_limit'][MESSAGE_PRIVATE_DEFAULT_INDEX]['default_interval'] = [
        '#type' => 'textfield',
        '#title' => t('Interval'),
-       '#default_value' => variable_get(MESSAGE_PRIVATE_DEFAULT_INTERVAL),
+       '#default_value' => $config->get('default_interval'),
        '#description' => t('Enter an interval in minutes') . ' ' . MESSAGE_PRIVATE_MESSAGE_INTERVAL_MIN . ' - ' . MESSAGE_PRIVATE_MESSAGE_INTERVAL_MAX,
-     );
+     ];
 
 
     // Generate variable names for all roles used with get/set in admin form.
     foreach (user_roles() as $id => $role) {
-      $role_name = str_replace(' ', '_', $role);
+      $role_name = $role->id();
       $limit_name = 'message_private_' . $role_name . '_limit';
       $interval_name = 'message_private_' . $role_name . '_interval';
 
       $form['interval_limit'][$id] = array(
         '#type' => 'fieldset',
-        '#title' => $role,
+        '#title' => $role->label(),
         '#collapsible' => TRUE,
         '#collapsed' => TRUE,
       );
@@ -160,14 +160,14 @@ class SettingsForm extends ConfigFormBase {
       $form['interval_limit'][$id][$limit_name] = array(
         '#type' => 'textfield',
         '#title' => t('Limit'),
-        '#default_value' => variable_get($limit_name),
+        '#default_value' => $config->get($limit_name),
         '#description' => t('Enter a message limit') . ' ' . MESSAGE_PRIVATE_MESSAGE_LIMIT_MIN . ' - ' . MESSAGE_PRIVATE_MESSAGE_LIMIT_MAX,
       );
 
       $form['interval_limit'][$id][$interval_name] = array(
         '#type' => 'textfield',
         '#title' => t('Interval'),
-        '#default_value' => variable_get($interval_name),
+        '#default_value' => $config->get($interval_name),
         '#description' => t('Enter an interval in minutes') . ' ' . MESSAGE_PRIVATE_MESSAGE_INTERVAL_MIN . ' - ' . MESSAGE_PRIVATE_MESSAGE_INTERVAL_MAX,
       );
     }
@@ -179,10 +179,27 @@ class SettingsForm extends ConfigFormBase {
    * {@inheritdoc}
    */
   public function submitForm(array &$form, FormStateInterface $form_state) {
-    $config = $this->config('message_private.settings');
+    /* @var $config \Drupal\Core\Config\Config|\Drupal\Core\Config\ImmutableConfig */
+    $config = \Drupal::service('config.factory')->getEditable('message_private.settings');
 
-    foreach ($this->defaultKeys() as $key) {
-      $config->set($key, $form_state->getValue($key));
+    // Init limit and interval keys.
+    $limit_names = [];
+    $interval_names = [];
+
+    // Cycle through each role and set up valid keys.
+    foreach (user_roles() as $id => $role) {
+      $role_name = $role->id();
+      $limit_names[] = 'message_private_' . $role_name . '_limit';
+      $interval_names[] = 'message_private_' . $role_name . '_interval';
+    }
+
+    foreach ($form_state->getValues() as $key => $value) {
+      // Store only default setting or dynamic valid key values.
+      if (in_array($key, $this->defaultKeys())
+        || in_array($key, $limit_names)
+        || in_array($key, $interval_names)) {
+        $config->set($key, $form_state->getValue($key));
+      }
     }
 
     $config->save();
@@ -194,19 +211,17 @@ class SettingsForm extends ConfigFormBase {
    * {@inheritdoc}
    */
   public function validateForm(array &$form, FormStateInterface $form_state) {
-    if (strlen($form_state->getValue('phone_number')) < 3) {
-      $form_state->setErrorByName('phone_number', $this->t('The phone number is too short. Please enter a full phone number.'));
-    }
     // Validate the default fieldset values.
     $this->validateFieldset($form_state,
-      $form['interval_limit'][MESSAGE_PRIVATE_DEFAULT_INDEX][MESSAGE_PRIVATE_DEFAULT_LIMIT],
-      $form['interval_limit'][MESSAGE_PRIVATE_DEFAULT_INDEX][MESSAGE_PRIVATE_DEFAULT_INTERVAL],
-      $form_state->getValue(MESSAGE_PRIVATE_DEFAULT_LIMIT),
-      $form_state->getValue(MESSAGE_PRIVATE_DEFAULT_INTERVAL)
+      $form['interval_limit'][MESSAGE_PRIVATE_DEFAULT_INDEX]['default_limit'],
+      $form['interval_limit'][MESSAGE_PRIVATE_DEFAULT_INDEX]['default_interval'],
+      $form_state->getValue('default_limit'),
+      $form_state->getValue('default_interval')
     );
+
     // Cycle through the settings for each role and validate.
     foreach (user_roles() as $id => $role) {
-      $role_name = str_replace(' ', '_', $role);
+      $role_name = $role->id();
       $limit_name = 'message_private_' . $role_name . '_limit';
       $interval_name = 'message_private_' . $role_name . '_interval';
 
@@ -214,8 +229,8 @@ class SettingsForm extends ConfigFormBase {
         $form_state,
         $form['interval_limit'][$id][$limit_name],
         $form['interval_limit'][$id][$interval_name],
-        $form_state['values'][$limit_name],
-        $form_state['values'][$interval_name]
+        $form_state->getValue($limit_name),
+        $form_state->getValue($interval_name)
       );
     }
   }
